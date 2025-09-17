760257c722548805495fd4a9724cc456ac3a1bab
0xLeonardoChahud <leonardo.chahud.dev@gmail.com>
2025-09-17 14:54:39 -0300

    +Adding color for tips

--- CT-208/Suguru/Suguru.py
+++ CT-208/Suguru/Suguru.py
@@ -16,6 +16,7 @@ class Suguru:
         self.grid, self.regions = grid, regions
         self.rows, self.cols = self.grid.shape
         self.size = self.rows*self.cols
+        self.tips = np.argwhere(self.grid != 0)
 
         self.solver = solver(self.grid, self.regions, delay)
 
@@ -39,36 +40,15 @@ class Suguru:
 
     def _poll_updates(self, delay):
         #self.solver._update_main_grid()
-        self.grid = self.solver.grid
+        self.grid = self.solver.grid.copy()
+        self.tips = self.solver.tips.copy()
         self.gui.set_grid(self.grid)
+        self.gui.set_tips(self.tips)
 
         if SuguruSolvers.Checker.solved(self.grid, self.regions):
             self.gui.set_solved()
         self.root.after(delay, lambda: self._poll_updates(delay))
 
-def parse_suguru_line_to_grid_and_regions(line):
-    data = json.loads(line)
-    rows, cols = data["rows"], data["cols"]
-
-    # Create empty grid
-    grid = np.zeros((rows, cols), dtype=int)
-    regions = np.zeros((rows, cols), dtype=int)
-
-    # Fill grid
-    cells_str = data["cells"]
-    for idx in range(rows * cols):
-        val = int(cells_str[2*idx])
-        row, col = divmod(idx, cols)
-        grid[row, col] = val
-
-    # Fill regions
-    for region_id, group_str in enumerate(data["groups"]):
-        for i in range(0, len(group_str), 2):
-            cell_idx = int(group_str[i:i+2])
-            r, c = divmod(cell_idx, cols)
-            regions[r, c] = region_id
-
-    return grid, regions
 
 
 def parse_suguru_binary(path):
@@ -82,18 +62,6 @@ def parse_suguru_binary(path):
         arr = np.fromfile(fp, dtype=np.int16).reshape(3, rows, cols)
     return arr
 
-def parse_json_file(path):
-    path = 'puzzles.json'
-    puzzles = list()
-    with open(path, "r") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            grid, regions = parse_suguru_line_to_grid_and_regions(line)
-            puzzles.append((grid, regions))
-    return puzzles
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', help='Path to Suguru file')
@@ -103,7 +71,7 @@ def main():
     grid, solution, regions = parse_suguru_binary(
         path
     )
-    s = Suguru(grid, regions, SASolver.SASolver, 0.01)
+    s = Suguru(grid, regions, SASolver.SASolver, 0.001)
     s.solve()
     s.show()
 
